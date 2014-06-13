b2559ed866462b4398e0a796d2e11d0aabfbfc11
tibor19 <tibi@covaci.se>
2014-06-13 14:59:03 -0700

    async/await

--- FastFasterAsyncWebForms/DisplayRecipe.aspx.cs
+++ FastFasterAsyncWebForms/DisplayRecipe.aspx.cs
@@ -2,25 +2,25 @@
 using System.Configuration;
 using System.Data.SqlClient;
 using System.Diagnostics;
-using System.Web.UI;
+using System.Threading.Tasks;
 
 namespace FastFasterAsyncWebForms
 {
     public partial class DisplayRecipe : System.Web.UI.Page
     {
         Stopwatch _stopWatch;
-        protected void Page_Load(object sender, EventArgs e)
+        protected async void Page_Load(object sender, EventArgs e)
         {
             _stopWatch = Stopwatch.StartNew();
             lblError.Text = string.Empty;
 
-            int recipeId = 0;
+            var recipeId = 0;
             if (Int32.TryParse(Request.QueryString["ID"], out recipeId) && recipeId != 0)
             {
-                lblMethod.Text = "RegisterAsyncTask";
-                GetDetails(recipeId);
-                GetIngredients(recipeId);
-                GetInstructions(recipeId);
+                lblMethod.Text = "async/await";
+                await GetDetailsAsync(recipeId);
+                await GetIngredientsAsync(recipeId);
+                await GetInstructionsAsync(recipeId);
             }
         }
 
@@ -31,86 +31,60 @@ namespace FastFasterAsyncWebForms
             base.OnPreRenderComplete(e);
         }
 
-        private void GetDetails(int recipeId)
+        private async Task GetDetailsAsync(int recipeId)
         {
-            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RecipesDBConnectionString"].ConnectionString);
+            var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RecipesDBConnectionString"].ConnectionString);
             conn.Open();
 
-            SqlCommand cmd = new SqlCommand("GetRecipeDetails", conn);
+            var cmd = new SqlCommand("GetRecipeDetails", conn);
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
             cmd.Parameters.AddWithValue("@RecipeID", recipeId);
 
-            // for ASP.NET 2.0 applications use delegates instead of lamdba expressions
-            Page.RegisterAsyncTask(new PageAsyncTask(
-                (sender, eventArg, callback, state) => cmd.BeginExecuteReader(callback, state), // beginHandler
-                ar => { // endHandler
-
-                    using (SqlDataReader reader = cmd.EndExecuteReader(ar))
-                    {
-                        if (reader.Read())
-                        {
-                            RecipeName.Text = reader.GetString(0);
-                            RecipeImage.ImageUrl = @"Images/food/" + reader.GetString(1) + ".jpg";
-                            RecipePrice.Text = reader.GetDecimal(2).ToString();
-                            RecipeTime.Text = reader.GetInt32(3).ToString();
-                        }
-                    }
-                    conn.Close();
-                },
-                null, // timeoutHandler
-                null, // state
-                true // executeInParallel
-                ));
+            using (var reader = await cmd.ExecuteReaderAsync())
+            {
+                if (reader.Read())
+                {
+                    RecipeName.Text = reader.GetString(0);
+                    RecipeImage.ImageUrl = @"Images/food/" + reader.GetString(1) + ".jpg";
+                    RecipePrice.Text = reader.GetDecimal(2).ToString();
+                    RecipeTime.Text = reader.GetInt32(3).ToString();
+                }
+            }
+            conn.Close();
         }
 
-        private void GetIngredients(int recipeId)
+        private async Task GetIngredientsAsync(int recipeId)
         {
-            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RecipesDBConnectionString"].ConnectionString);
+            var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RecipesDBConnectionString"].ConnectionString);
             conn.Open();
 
-            SqlCommand cmd = new SqlCommand("GetIngredientsForRecipe", conn);
+            var cmd = new SqlCommand("GetIngredientsForRecipe", conn);
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
             cmd.Parameters.AddWithValue("@RecipeID", recipeId);
 
-            Page.RegisterAsyncTask(new PageAsyncTask(
-                (sender, eventArg, callback, state) => cmd.BeginExecuteReader(callback, state), // beginHandler
-                ar => { // endHandler
-                    using (SqlDataReader reader = cmd.EndExecuteReader(ar))
-                    {
-                        lstRecipeIngredients.DataSource = reader;
-                        lstRecipeIngredients.DataBind();
-                    }
-                    conn.Close();
-                },
-                null, // timeoutHandler
-                null, // state
-                true // executeInParallel
-                ));
+            using (var reader = await cmd.ExecuteReaderAsync())
+            {
+                lstRecipeIngredients.DataSource = reader;
+                lstRecipeIngredients.DataBind();
+            }
+            conn.Close();
         }
 
-        private void GetInstructions(int recipeId)
+        private async Task GetInstructionsAsync(int recipeId)
         {
-            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RecipesDBConnectionString"].ConnectionString);
+            var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RecipesDBConnectionString"].ConnectionString);
             conn.Open();
 
-            SqlCommand cmd = new SqlCommand("GetInstructionsForRecipe", conn);
+            var cmd = new SqlCommand("GetInstructionsForRecipe", conn);
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
             cmd.Parameters.AddWithValue("@RecipeID", recipeId);
 
-            Page.RegisterAsyncTask(new PageAsyncTask(
-                (sender, eventArg, callback, state) => cmd.BeginExecuteReader(callback, state), // beginHandler
-                ar => { // endHandler
-                    using (SqlDataReader reader = cmd.EndExecuteReader(ar))
-                    {
-                        lstRecipeInstructions.DataSource = reader;
-                        lstRecipeInstructions.DataBind();
-                    }
-                    conn.Close();
-                },
-                null, // timeoutHandler
-                null, // state
-                true // executeInParallel
-                ));
+            using (var reader = await cmd.ExecuteReaderAsync())
+            {
+                lstRecipeInstructions.DataSource = reader;
+                lstRecipeInstructions.DataBind();
+            }
+            conn.Close();
         }
     }
 }
